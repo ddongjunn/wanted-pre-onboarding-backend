f0a7594e0a0d2997d4daf62443c7067bfaf20a8c
ddongjunn <ddongjunn@gmail.com>
2023-10-13 18:35:30 +0900

    fix : 불필요한 import 제거

--- src/main/java/com/api/employment/domain/company/repository/CompanyRepository.java
+++ src/main/java/com/api/employment/domain/company/repository/CompanyRepository.java
@@ -4,8 +4,6 @@ import com.api.employment.domain.company.entity.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface CompanyRepository extends JpaRepository<Company, Long> {
 
--- src/main/java/com/api/employment/domain/company/service/CompanyService.java
+++ src/main/java/com/api/employment/domain/company/service/CompanyService.java
@@ -2,7 +2,6 @@ package com.api.employment.domain.company.service;
 
 import com.api.employment.domain.common.error.ErrorCode;
 import com.api.employment.domain.common.error.SuccesCode;
-import com.api.employment.domain.company.entity.Company;
 import com.api.employment.domain.company.model.CompanyResponseDTO;
 import com.api.employment.domain.company.model.CompanySaveRequestDTO;
 import com.api.employment.domain.company.repository.CompanyRepository;
@@ -10,8 +9,6 @@ import com.api.employment.domain.common.error.exception.CustomException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Slf4j
 @Service
