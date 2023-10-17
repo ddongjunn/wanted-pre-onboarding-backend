46e272fab1c51f0e4c91aaa2e3498347c2020f07
ddongjunn <ddongjunn@gmail.com>
2023-10-17 17:56:33 +0900

    feat : logging aop 작업중

---
+++ src/main/java/com/api/employment/common/logging/LoggableController.java
@@ -0,0 +1,2 @@
+package com.api.employment.common.logging;public @interface loggableController {
+}
---
+++ src/main/java/com/api/employment/common/logging/LoggingAspect.java
@@ -0,0 +1,12 @@
+package com.api.employment.common.logging;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+@Slf4j
+public class LoggingAspect {
+
+}
